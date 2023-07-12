9061438919315c38aab1eee968f3863e925945e0
Will3000 <miguelcarter@gmail.com>
2023-07-12 16:40:56 +0200

    update all vasr.yaml

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Archivos despliegue de ansible"
 
-#cd /home/miguel/Documentos/Proyectos/Practica_2/terraform
-#/home/miguel/Documentos/Proyectos/Practica_2/terraform/terra.sh
+cd /home/miguel/Documentos/Proyectos/Practica_2/terraform
+/home/miguel/Documentos/Proyectos/Practica_2/terraform/terra.sh
 
-#cd /home/miguel/Documentos/Proyectos/Practica_2/ansible
-#ansible-playbook -i inventory playbook_obtenerlogs.yaml
+cd /home/miguel/Documentos/Proyectos/Practica_2/ansible
+ansible-playbook -i inventory playbook_obtenerlogs.yaml
 
-#chmod +x vars.sh
-#./vars.sh
+chmod +x vars.sh
+./vars.sh
 
-#ansible-playbook -i inventory playbook_instalar.yaml
-#ansible-playbook -i inventory playbook_copiar.yaml
-#ansible-playbook -i inventory playbook_certificados.yaml
+ansible-playbook -i inventory playbook_instalar.yaml
+ansible-playbook -i inventory playbook_copiar.yaml
+ansible-playbook -i inventory playbook_certificados.yaml
 ansible-playbook -i inventory playbook_podmanlogin.yaml
 ansible-playbook -i inventory playbook_images.yaml
 
