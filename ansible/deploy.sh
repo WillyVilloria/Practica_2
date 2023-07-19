b3796da2a874ff2558117347639201502a5118ac
Will3000 <miguelcarter@gmail.com>
2023-07-19 19:58:06 +0200

    update k8s 19/07

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -10,14 +10,13 @@ echo "Archivos despliegue de ansible"
 #chmod +x vars.sh
 #./dep_vars.sh
 
+#ansible-playbook -i inventory playbook_instalarpodman.yaml
 #ansible-playbook -i inventory playbook_instalar.yaml
 #ansible-playbook -i inventory playbook_copiar.yaml
 #ansible-playbook -i inventory playbook_certificados.yaml
-ansible-playbook -i inventory playbook_podmanlogin.yaml
+#ansible-playbook -i inventory playbook_podmanlogin.yaml
 #ansible-playbook -i inventory playbook_images.yaml
-ansible-playbook -i inventory playbook_imagefor_k8s.yaml
-#ansible-playbook -i inventory playbook_autenticacion_k8s.yaml
-
-
+#ansible-playbook -i inventory playbook_imagefor_k8s.yaml
+ansible-playbook -i inventory playbook_autenticacion_k8s.yaml
 
 #ansible-playbook -i inventory playbook_borrarservicio.yaml
